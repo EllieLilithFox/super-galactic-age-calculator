3794206afc159b99ca37e21e64acc43a36c22d57
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-26 13:03:56 -0700

    add more to calculateLifeLeft method and failing sixth failing test

--- src/age-calculator.js
+++ src/age-calculator.js
@@ -23,4 +23,22 @@ export class Age {
   calculateJupiter() {
     this.ageJupiter = Math.round(this.age / 11.86);
   }
+
+  calculateLifeLeft() {
+    this.calculateMercury();
+    this.calculateVenus();
+    this.calculateMars();
+    this.calculateJupiter();
+
+    let mercuryLifeLeft = Math.round(this.lifeExpectancy / .24) - this.ageMercury;
+    let venusLifeLeft = Math.round(this.lifeExpectancy / .62) - this.ageVenus;
+    let marsLifeLeft = Math.round(this.lifeExpectancy / 1.88) - this.ageMars;
+    let jupiterLifeLeft = Math.round(this.lifeExpectancy / 11.86) - this.ageJupiter;
+
+    if (this.age < this.lifeExpectancy) {
+      return ``;
+    } else {
+      return ``;
+    }
+  }
 }

7a91a9ed58b8aaaea86d778e3419cf1dc2960b8d
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-26 09:22:10 -0700

    fix test file name and add first failing test

---
+++ __test__/age-calculator.test.js
@@ -0,0 +1,19 @@
+import { Age } from "../src/age-calculator";
+
+describe("Age", () => {
+  let reusableAge;
+
+  beforeEach(() => {
+    reusableAge = new Age();
+  })
+
+  test("should create an age object with correct values", () => {
+    expect(reusableAge.age).toBe(42);
+    expect(reusableAge.lifeExpectancy).toBe(99);
+    expect(reusableAge.ageMercury).toBe(0);
+    expect(reusableAge.ageVenus).toBe(0);
+    expect(reusableAge.ageMars).toBe(0);
+    expect(reusableAge.ageJupiter).toBe(0);
+  })
+  
+});
---
+++ src/age-calculator.js
@@ -0,0 +1,10 @@
+export class Age {
+  constructor() {
+    this.age = "";
+    this.lifeExpectancy = "";
+    this.ageMercury = "";
+    this.ageVenus = "";
+    this.ageMars = "";
+    this.ageJupiter = "";
+  }
+}

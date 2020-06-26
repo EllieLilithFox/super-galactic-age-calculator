60966c1b7de5690f3c5fd152f6ee9cf1b5c5e025
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-26 15:07:57 -0700

    refactor methods remove unnessicary methods test 7 fails

--- src/age-calculator.js
+++ src/age-calculator.js
@@ -2,48 +2,24 @@ export class Age {
   constructor(age, lifeExpectancy) {
     this.age = age;
     this.lifeExpectancy = lifeExpectancy;
-    this.ageMercury = 0;
-    this.ageVenus = 0;
-    this.ageMars = 0;
-    this.ageJupiter = 0;
-    this.ageProxima = 0;
-  }
-
-  calculateMercury() {
     this.ageMercury = Math.round(this.age / .24);
-  }
-
-  calculateVenus() {
     this.ageVenus = Math.round(this.age / .62);
-  }
-
-  calculateMars() {
     this.ageMars = Math.round(this.age / 1.88);
-  }
-
-  calculateJupiter() {
     this.ageJupiter = Math.round(this.age / 11.86);
-  }
-
-  calculateProxima() {
     this.ageProxima = Math.round(this.age / .03);
   }
 
   calculateLifeLeft() {
-    this.calculateMercury();
-    this.calculateVenus();
-    this.calculateMars();
-    this.calculateJupiter();
-
     let mercuryLifeLeft = Math.abs(Math.round(this.lifeExpectancy / .24) - this.ageMercury);
     let venusLifeLeft = Math.abs(Math.round(this.lifeExpectancy / .62) - this.ageVenus);
     let marsLifeLeft = Math.abs(Math.round(this.lifeExpectancy / 1.88) - this.ageMars);
     let jupiterLifeLeft = Math.abs(Math.round(this.lifeExpectancy / 11.86) - this.ageJupiter);
+    let proximaLifeLeft = Math.abs(Math.round(this.lifeExpectancy / .03) - this.ageProxima)
 
     if (this.age < this.lifeExpectancy) {
-      return `You have ${mercuryLifeLeft} Mercury Years, ${venusLifeLeft} Venus years, ${marsLifeLeft} Mars years and ${jupiterLifeLeft} Jupiter years left to live.`;
+      return `You have ${mercuryLifeLeft} Mercury Years, ${venusLifeLeft} Venus years, ${marsLifeLeft} Mars years, ${jupiterLifeLeft} Jupiter years and ${proximaLifeLeft} Proxima Centauri b years left to live.`;
     } else {
-      return `You have lived ${mercuryLifeLeft} Mercury Years, ${venusLifeLeft} Venus years, ${marsLifeLeft} Mars years and ${jupiterLifeLeft} Jupiter years over your live expectancy.`;
+      return `You have lived ${mercuryLifeLeft} Mercury Years, ${venusLifeLeft} Venus years, ${marsLifeLeft} Mars years, ${jupiterLifeLeft} Jupiter years and ${proximaLifeLeft} Proxima Centauri b years over your live expectancy.`;
     }
   }
 }

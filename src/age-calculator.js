befbb45d2473c969c32ddf97f013b00a2a9b94da
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-26 16:09:34 -0700

    add calcLightYear method and failing test 8

--- src/age-calculator.js
+++ src/age-calculator.js
@@ -10,16 +10,40 @@ export class Age {
   }
 
   calculateLifeLeft() {
-    let mercuryLifeLeft = Math.abs(Math.round(this.lifeExpectancy / .24) - this.ageMercury);
-    let venusLifeLeft = Math.abs(Math.round(this.lifeExpectancy / .62) - this.ageVenus);
-    let marsLifeLeft = Math.abs(Math.round(this.lifeExpectancy / 1.88) - this.ageMars);
-    let jupiterLifeLeft = Math.abs(Math.round(this.lifeExpectancy / 11.86) - this.ageJupiter);
-    let proximaLifeLeft = Math.abs(Math.round(this.lifeExpectancy / .03) - this.ageProxima);
+    const mercuryLifeLeft = Math.abs(Math.round(this.lifeExpectancy / .24) - this.ageMercury);
+    const venusLifeLeft = Math.abs(Math.round(this.lifeExpectancy / .62) - this.ageVenus);
+    const marsLifeLeft = Math.abs(Math.round(this.lifeExpectancy / 1.88) - this.ageMars);
+    const jupiterLifeLeft = Math.abs(Math.round(this.lifeExpectancy / 11.86) - this.ageJupiter);
+    const proximaLifeLeft = Math.abs(Math.round(this.lifeExpectancy / .03) - this.ageProxima);
 
     if (this.age < this.lifeExpectancy) {
       return `You have ${mercuryLifeLeft} Mercury Years, ${venusLifeLeft} Venus years, ${marsLifeLeft} Mars years, ${jupiterLifeLeft} Jupiter years and ${proximaLifeLeft} Proxima Centauri b years left to live.`;
     } else {
       return `You have lived ${mercuryLifeLeft} Mercury Years, ${venusLifeLeft} Venus years, ${marsLifeLeft} Mars years, ${jupiterLifeLeft} Jupiter years and ${proximaLifeLeft} Proxima Centauri b years over your live expectancy.`;
     }
+  }
+
+  // age / 6.9
+  calcLightYear(planet) {
+    const lightSpeedAge = Math.round(this.age / 6.9);
+    let planetLightSpeedAge = 0;
+    switch (planet) {
+    case "Mercury":
+      planetLightSpeedAge = Math.round(lightSpeedAge / .24);
+      break;
+    case "Venus":
+      planetLightSpeedAge = Math.round(lightSpeedAge / .62);
+      break;
+    case "Mars":
+      planetLightSpeedAge = Math.round(lightSpeedAge / 1.88);
+      break;
+    case "Jupiter":
+      planetLightSpeedAge = Math.round(lightSpeedAge / 11.86);
+      break;
+    default:
+      planetLightSpeedAge = Math.round(lightSpeedAge / .03);
+    }
+
+    return ``;
   }
 }

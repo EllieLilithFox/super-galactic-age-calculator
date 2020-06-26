e8995e855092ca0821c657159553b83112ea83cb
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-26 15:31:21 -0700

    test 7 passes again

--- src/age-calculator.js
+++ src/age-calculator.js
@@ -14,7 +14,7 @@ export class Age {
     let venusLifeLeft = Math.abs(Math.round(this.lifeExpectancy / .62) - this.ageVenus);
     let marsLifeLeft = Math.abs(Math.round(this.lifeExpectancy / 1.88) - this.ageMars);
     let jupiterLifeLeft = Math.abs(Math.round(this.lifeExpectancy / 11.86) - this.ageJupiter);
-    let proximaLifeLeft = Math.abs(Math.round(this.lifeExpectancy / .03) - this.ageProxima)
+    let proximaLifeLeft = Math.abs(Math.round(this.lifeExpectancy / .03) - this.ageProxima);
 
     if (this.age < this.lifeExpectancy) {
       return `You have ${mercuryLifeLeft} Mercury Years, ${venusLifeLeft} Venus years, ${marsLifeLeft} Mars years, ${jupiterLifeLeft} Jupiter years and ${proximaLifeLeft} Proxima Centauri b years left to live.`;
